7dd86306189ba9264876080b76bfeb4c4ca70a43
Matt <58913953+ms5984@users.noreply.github.com>
2021-10-02 01:59:30 -0400

    Add new utility as static factory * Plugin serves as ItemStack cache

--- src/main/java/itzshmulik/survivelist/survivelistdisguises/SurvivelistDisguises.java
+++ src/main/java/itzshmulik/survivelist/survivelistdisguises/SurvivelistDisguises.java
@@ -1,13 +1,26 @@
 package itzshmulik.survivelist.survivelistdisguises;
 
+import itzshmulik.survivelist.survivelistdisguises.util.HeadGrabber;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import itzshmulik.survivelist.survivelistdisguises.commands.DisguiseCommand;
 import itzshmulik.survivelist.survivelistdisguises.events.MenuListener;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.UUID;
+
 public final class SurvivelistDisguises extends JavaPlugin {
+
+    private static SurvivelistDisguises instance;
+    private final HashMap<String, ItemStack> headsByTexture = new HashMap<>();
+    private final HashMap<UUID, ItemStack> headsByPlayer = new HashMap<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
 
         getCommand("disguise").setExecutor(new DisguiseCommand());
 
@@ -16,5 +29,45 @@ public final class SurvivelistDisguises extends JavaPlugin {
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
+    }
+
+    @Override
+    public void onDisable() {
+        // clear caches
+        headsByTexture.clear();
+        headsByPlayer.clear();
+    }
+
+    public static ItemStack headByTexture(@NotNull String textureValue) {
+        final ItemStack itemStack = instance.headsByTexture.get(textureValue);
+        if (itemStack != null) return new ItemStack(itemStack);
+        final ItemStack generated = new HeadGrabber.CustomTexture(textureValue).generate();
+        synchronized (instance.headsByTexture) {
+            instance.headsByTexture.put(textureValue, generated);
+        }
+        return new ItemStack(generated);
+    }
+
+    public static ItemStack headByTextureWithName(@NotNull String textureValue, String itemName) {
+        if (itemName == null) return headByTexture(textureValue);
+        final String cacheKey = textureValue + ":" + itemName;
+        final ItemStack itemStack = instance.headsByTexture.get(cacheKey);
+        if (itemStack != null) return new ItemStack(itemStack);
+        final ItemStack generated = new HeadGrabber.CustomTexture(textureValue, itemName).generate();
+        synchronized (instance.headsByTexture) {
+            instance.headsByTexture.put(cacheKey, generated);
+        }
+        return new ItemStack(generated);
+    }
+
+    public static ItemStack headByPlayer(@NotNull OfflinePlayer player) {
+        final UUID uniqueId = player.getUniqueId();
+        final ItemStack itemStack = instance.headsByPlayer.get(uniqueId);
+        if (itemStack != null) return new ItemStack(itemStack);
+        final ItemStack generated = new HeadGrabber.Player(player).generate();
+        synchronized (instance.headsByTexture) {
+            instance.headsByPlayer.put(uniqueId, generated);
+        }
+        return new ItemStack(generated);
     }
 }
